7d867a3148464fcbdd0a5a1ba2d8d6124d013084
Elena <lenagalkovskaya@yandex.ru>
2022-12-22 11:11:01 +0300

    commit Task_3

--- Task3.py
+++ Task3.py
@@ -3,6 +3,6 @@
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-lst = list(map(float, input("Введите числа через пробел: ").split()))
-new_lst = [round(i%1,2) for i in lst if i%1 != 0]
-print(max(new_lst) - min(new_lst))
+list = list(map(float, input("Введите числа через пробел: ").split()))
+new_list = [round(i%1,2) for i in list if i%1 != 0]
+print(max(new_list) - min(new_list))
